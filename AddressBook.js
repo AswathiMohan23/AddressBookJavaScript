06aac24d8fbfc35015ef62597ad8031b0ace2fdb
AswathiMohan23 <achu999here@gmail.com>
2022-12-18 23:28:19 +0530

    [ADDED] : code for firstName validation using regex pattern

--- AddressBook.js
+++ AddressBook.js
@@ -10,14 +10,31 @@ class AddressBook{
         this.email=params[7];
     }
     
+    /**
+     * @param {string} name
+     */
+    set setName(name){
+        let nameRegex=RegExp("[A-Z]{1}[a-z]{3,}$")
+        if(nameRegex.test(name)){
+            this.name=name;
+        }else
+            throw "Name '"+name+"' is incorrect";
+    }
+
     toString(){
         return "firstName = "+this.firstName+" , secondName = "+this.lastName+" ,address = "+this.address+" ,city = "+this.city
                     +" , state = "+this.state+" , zip = "+this.zip+" ,phoneNo = "+this.phoneNo+" , email = "+this.email;
     } 
 }
-
+let addressBook =new AddressBook("Kiran","Nair","hij villa")
+console.log(addressBook.toString());
 let addressBook1 =new AddressBook("Tom","John","abc villa","Bangalore","Karnataka","456789","942357891","tom@gmail.com");
 console.log(addressBook1.toString());
 let addressBook2 =new AddressBook("Anna","Maria","pqr apartment","Mysore","Karnataka","766789","942357123","annmaria@gmail.com");
 console.log(addressBook2.toString());
-
+try{
+    addressBook1.setName="daisy" //throws an error because id must be a non-zero number
+    console.log(addressBook1.toString())
+}catch(e){
+    console.error(e);
+}
